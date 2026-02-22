195b27ce33b8eefe9925505223c2b2b846c91576
Daniel Swarbrick <daniel.swarbrick@gmail.com>
2026-02-22 10:49:16 +0100

    Initial check-in

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/alecthomas/kingpin/v2"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/common/promslog"
+	"github.com/prometheus/common/promslog/flag"
+	"github.com/prometheus/common/version"
+	"github.com/prometheus/exporter-toolkit/web"
+	"github.com/prometheus/exporter-toolkit/web/kingpinflag"
+)
+
+func main() {
+	var (
+		metricsPath = kingpin.Flag(
+			"web.telemetry-path", "Path under which to expose metrics.",
+		).Default("/metrics").String()
+		toolkitFlags = kingpinflag.AddFlags(kingpin.CommandLine, ":9654")
+	)
+
+	promslogConfig := &promslog.Config{}
+	flag.AddFlags(kingpin.CommandLine, promslogConfig)
+	kingpin.Version(version.Print("ceph_exporter"))
+	kingpin.CommandLine.UsageWriter(os.Stdout)
+	kingpin.HelpFlag.Short('h')
+	kingpin.Parse()
+	logger := promslog.New(promslogConfig)
+
+	http.Handle(*metricsPath, promhttp.Handler())
+
+	if *metricsPath != "/" {
+		landingConfig := web.LandingConfig{
+			Name:        "Ceph Exporter",
+			Description: "Prometheus Ceph Exporter",
+			HeaderColor: "#f0424d",
+			Version:     version.Info(),
+			Links: []web.LandingLinks{
+				{
+					Address: *metricsPath,
+					Text:    "Metrics",
+				},
+			},
+		}
+		landingPage, err := web.NewLandingPage(landingConfig)
+		if err != nil {
+			logger.Error(err.Error())
+			os.Exit(1)
+		}
+		http.Handle("/", landingPage)
+	}
+
+	if err := web.ListenAndServe(&http.Server{}, toolkitFlags, logger); err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+}
